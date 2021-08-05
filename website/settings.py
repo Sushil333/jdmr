e785c3210838444ee00668a5f1fe041fc6d5c7e9
Sushil333 <sushilbhardwaj705@gmail.com>
2021-08-05 22:58:30 +0530

    UPDATE

--- website/settings.py
+++ website/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-lfz21&+(_5ke-d&^k#6=a(*7$wa-3+kvi*8nj-@cb%o!5le+7s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
