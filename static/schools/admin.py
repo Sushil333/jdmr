9fba981fda0e0c1722e6da5c1d236d70cb3ff64c
Sushil333 <sushilbhardwaj705@gmail.com>
2021-08-05 21:29:47 +0530

    update

--- static/schools/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from schools.models import SchoolsImportFile, School, SchoolDetail
-
-admin.site.register(SchoolDetail)
-admin.site.register(School)
-admin.site.register(SchoolsImportFile)
--- static/schools/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-from schools.models import SchoolsImportFile
-
-class ImportFileForm(forms.ModelForm):
-    class Meta:
-        model = SchoolsImportFile
-        fields = ('schools_import',)
